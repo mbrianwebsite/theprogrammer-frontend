fff6937833bbdc9f17f8ef865bf061e6e7fe451b
MBrian Website <muhammad.brian.27@gmail.com>
2023-02-11 09:52:39 +0700

    FIX HAMBURGER ICON

--- components/navbar/hamburger.vue
+++ components/navbar/hamburger.vue
@@ -1,8 +1,19 @@
+<script setup>
+defineProps({
+    hamburger: Boolean
+})
+</script>
 <template>
     <div class="flex flex-col justify-between 
     md:hidden">
-        <span class="w-8 h-[2px] bg-white"></span>
-        <span class="w-8 h-[2px] bg-white"></span>
-        <span class="w-8 h-[2px] bg-white"></span>
+        <span v-if="hamburger" class="w-8 h-[2px] bg-white"></span>
+        <span v-if="hamburger" class="w-8 h-[2px] bg-white"></span>
+        <span v-if="hamburger" class="w-8 h-[2px] bg-white"></span>
+        <div v-if="!hamburger" class="text-white">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="white" viewBox="0 0 24 24" stroke-width="1.5"
+                stroke="currentColor" class="w-8 h-8">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
+            </svg>
+        </div>
     </div>
 </template>
--- components/navbar/index.vue
+++ components/navbar/index.vue
@@ -1,9 +1,13 @@
+<script setup>
+const hamburger = ref(true)
+</script>
+
 <template>
     <div class="bg-[#3F423733] flex flex-row justify-between px-8 py-7 mt-6 
     ">
         <NavbarLogo />
         <NavbarMainmenu />
         <NavbarButton title="Sign Up" :mini="false" />
-        <NavbarHamburger @click="$emit('drawerAction')" />
+        <NavbarHamburger @click="$emit('drawerAction'); hamburger = !hamburger" :hamburger="hamburger" />
     </div>
 </template>
